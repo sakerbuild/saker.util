45bb9653803a67c52eae4186f3c9b440139a3109
sipkab <10866741+Sipkab@users.noreply.github.com>
2023-08-19 15:47:31 +0200

    Fix consistency of comparator() result returned by navigable collections
    The comparator() function result of navigable collections should equal
    to the similarly retrieved comparator from navigable collections of the
    Java standard library.

--- test/src/testing/saker/build/tests/utils/ImmutableNavigableMapTest.java
+++ test/src/testing/saker/build/tests/utils/ImmutableNavigableMapTest.java
@@ -15,11 +15,15 @@
  */
 package testing.saker.build.tests.utils;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.NavigableMap;
 import java.util.TreeMap;
 
 import saker.util.ImmutableUtils;
+import saker.util.ObjectUtils;
 import testing.saker.SakerTest;
 import testing.saker.SakerTestCase;
 
@@ -27,46 +31,164 @@ import testing.saker.SakerTestCase;
 public class ImmutableNavigableMapTest extends SakerTestCase {
 	@Override
 	public void runTest(Map<String, String> parameters) {
-		NavigableMap<Integer, String> actualmap = new TreeMap<>();
-		actualmap.put(0, "0");
-		actualmap.put(1, "1");
-		actualmap.put(3, "3");
-		actualmap.put(4, "4");
-		actualmap.put(9, "9");
-		actualmap.put(10, "10");
-		actualmap.put(14, "13");
+		{
+			NavigableMap<Integer, String> actualmap = new TreeMap<>();
+			actualmap.put(0, "0");
+			actualmap.put(1, "1");
+			actualmap.put(3, "3");
+			actualmap.put(4, "4");
+			actualmap.put(9, "9");
+			actualmap.put(10, "10");
+			actualmap.put(14, "13");
+			NavigableMap<Integer, String> reversemap = new TreeMap<>(Comparator.reverseOrder());
+			reversemap.putAll(actualmap);
 
-		test(actualmap, ImmutableUtils.unmodifiableNavigableMap(actualmap), -5, 20);
-		test(actualmap, ImmutableUtils.makeImmutableNavigableMap(actualmap), -5, 20);
+			testMapRange(actualmap);
+			testMapRange(reversemap);
+		}
+
+		{
+			//singleton checks
+			NavigableMap<Integer, String> actualmap = new TreeMap<>();
+			actualmap.put(0, "0");
+			NavigableMap<Integer, String> reversemap = new TreeMap<>(Comparator.reverseOrder());
+			reversemap.putAll(actualmap);
+
+			assertMapEquality(actualmap, ImmutableUtils.singletonNavigableMap(0, "0", actualmap.comparator()));
+			assertMapEquality(reversemap, ImmutableUtils.singletonNavigableMap(0, "0", reversemap.comparator()));
+		}
 
 		assertEquals(ImmutableUtils.singletonNavigableMap("a", 1, String::compareToIgnoreCase).get("A"), 1);
 		assertEquals(ImmutableUtils.singletonNavigableMap("a", 1, String::compareToIgnoreCase).get("a"), 1);
+
+		assertMapEquality(new TreeMap<>(), ImmutableUtils.emptyNavigableMap(null));
+		assertMapEquality(Collections.emptyNavigableMap(), ImmutableUtils.emptyNavigableMap(null));
+		assertMapEquality(new TreeMap<>(Comparator.reverseOrder()),
+				ImmutableUtils.emptyNavigableMap(Comparator.reverseOrder()));
+		assertMapEquality(new TreeMap<>(Comparator.reverseOrder().reversed()),
+				ImmutableUtils.emptyNavigableMap(Comparator.reverseOrder().reversed()));
+		assertMapEquality(new TreeMap<>(Comparator.naturalOrder()),
+				ImmutableUtils.emptyNavigableMap(Comparator.naturalOrder()));
 	}
 
-	protected static void test(NavigableMap<Integer, String> actualmap, NavigableMap<Integer, String> map, int start,
-			int end) {
-		assertEquals(actualmap.keySet(), map.keySet());
-		assertEquals(actualmap.entrySet(), map.entrySet());
-		for (int i = start; i < end; i++) {
-			assertEquals(map.floorKey(i), actualmap.floorKey(i));
-			assertEquals(map.lowerKey(i), actualmap.lowerKey(i));
-			assertEquals(map.ceilingKey(i), actualmap.ceilingKey(i));
-			assertEquals(map.higherKey(i), actualmap.higherKey(i));
-			assertEquals(map.floorEntry(i), actualmap.floorEntry(i));
-			assertEquals(map.lowerEntry(i), actualmap.lowerEntry(i));
-			assertEquals(map.ceilingEntry(i), actualmap.ceilingEntry(i));
-			assertEquals(map.higherEntry(i), actualmap.higherEntry(i));
+	private static void testMapRange(NavigableMap<Integer, String> map) {
+		int start = -5;
+		int end = 20;
 
-			assertEquals(map.headMap(i, false), actualmap.headMap(i, false));
-			assertEquals(map.headMap(i, true), actualmap.headMap(i, true));
-			assertEquals(map.tailMap(i, false), actualmap.tailMap(i, false));
-			assertEquals(map.tailMap(i, true), actualmap.tailMap(i, true));
-			for (int j = i; j < end; j++) {
-				assertEquals(map.subMap(i, false, j, false), actualmap.subMap(i, false, j, false));
-				assertEquals(map.subMap(i, true, j, false), actualmap.subMap(i, true, j, false));
-				assertEquals(map.subMap(i, false, j, true), actualmap.subMap(i, false, j, true));
-				assertEquals(map.subMap(i, true, j, true), actualmap.subMap(i, true, j, true));
+		System.out.println("ImmutableNavigableMapTest.testMapRange() " + map + " cmp: " + map.comparator());
+
+		testMaps(map, ImmutableUtils.unmodifiableNavigableMap(map), start, end);
+		testMaps(map, ImmutableUtils.makeImmutableNavigableMap(map), start, end);
+
+		Integer[] keysarray = map.keySet().toArray(new Integer[0]);
+		String[] valsarray = map.values().toArray(new String[0]);
+
+		testMaps(map, ImmutableUtils.makeImmutableNavigableMap(keysarray, valsarray, map.comparator()), start, end);
+		testMaps(map, ImmutableUtils.makeImmutableNavigableMap(Arrays.asList(keysarray), Arrays.asList(valsarray),
+				map.comparator()), start, end);
+		testMaps(map, ImmutableUtils.unmodifiableNavigableMap(keysarray, valsarray, map.comparator()), start, end);
+		testMaps(map, ImmutableUtils.unmodifiableNavigableMap(Arrays.asList(keysarray), Arrays.asList(valsarray),
+				map.comparator()), start, end);
+	}
+
+	protected static void testMaps(NavigableMap<Integer, String> actualmap, NavigableMap<Integer, String> utilmap,
+			int start, int end) {
+		assertMapEquality(actualmap, utilmap);
+
+		for (int i = start; i < end; i++) {
+			try {
+				assertEquals(utilmap.floorKey(i), actualmap.floorKey(i));
+				assertEquals(utilmap.lowerKey(i), actualmap.lowerKey(i));
+				assertEquals(utilmap.ceilingKey(i), actualmap.ceilingKey(i));
+				assertEquals(utilmap.higherKey(i), actualmap.higherKey(i));
+				assertEquals(utilmap.floorEntry(i), actualmap.floorEntry(i));
+				assertEquals(utilmap.lowerEntry(i), actualmap.lowerEntry(i));
+				assertEquals(utilmap.ceilingEntry(i), actualmap.ceilingEntry(i));
+				assertEquals(utilmap.higherEntry(i), actualmap.higherEntry(i));
+
+				assertMapEquality(utilmap.headMap(i, false), actualmap.headMap(i, false));
+				assertMapEquality(utilmap.headMap(i, true), actualmap.headMap(i, true));
+				assertMapEquality(utilmap.tailMap(i, false), actualmap.tailMap(i, false));
+				assertMapEquality(utilmap.tailMap(i, true), actualmap.tailMap(i, true));
+			} catch (Throwable e) {
+				System.out.println("With value: " + i);
+				throw e;
 			}
+			for (int j = i; j < end; j++) {
+				int l = i;
+				int r = j;
+				Comparator<? super Integer> comparator = ObjectUtils.getComparator(utilmap);
+				if (comparator.compare(l, r) > 0) {
+					//use reverse order if the values compare otherwise
+					l = j;
+					r = i;
+				}
+
+				assertMapEquality(utilmap.subMap(l, false, r, false), actualmap.subMap(l, false, r, false));
+				assertMapEquality(utilmap.subMap(l, true, r, false), actualmap.subMap(l, true, r, false));
+				assertMapEquality(utilmap.subMap(l, false, r, true), actualmap.subMap(l, false, r, true));
+				assertMapEquality(utilmap.subMap(l, true, r, true), actualmap.subMap(l, true, r, true));
+			}
+		}
+	}
+
+	private static void assertMapEquality(NavigableMap<?, ?> left, NavigableMap<?, ?> right) {
+		try {
+			assertMapEqualityImpl(left, right);
+		} catch (Throwable e) {
+			System.out.println("Comparing map " + left + " (" + left.getClass().getSimpleName() + " / "
+					+ left.comparator() + ") - " + right + " (" + right.getClass().getSimpleName() + " / "
+					+ right.comparator() + ")");
+			throw e;
+		}
+
+		try {
+			assertMapEqualityImpl(left.descendingMap(), right.descendingMap());
+		} catch (Throwable e) {
+			System.out.println("Comparing descending map of " + left + " (" + left.getClass().getSimpleName() + " / "
+					+ left.comparator() + ") - " + right + " (" + right.getClass().getSimpleName() + " / "
+					+ right.comparator() + ")");
+			throw e;
+		}
+
+		try {
+			assertMapEqualityImpl(left.descendingMap().descendingMap(), right.descendingMap().descendingMap());
+		} catch (Throwable e) {
+			System.out.println("Comparing descending 2 map of " + left + " (" + left.getClass().getSimpleName() + " / "
+					+ left.comparator() + ") - " + right + " (" + right.getClass().getSimpleName() + " / "
+					+ right.comparator() + ")");
+			throw e;
+		}
+	}
+
+	private static void assertMapEqualityImpl(NavigableMap<?, ?> left, NavigableMap<?, ?> right) {
+		try {
+			boolean lempty = left.isEmpty();
+			assertEquals(lempty, right.isEmpty());
+			if (!lempty) {
+				assertEquals(left.firstKey(), right.firstKey());
+				assertEquals(left.lastKey(), right.lastKey());
+				assertEquals(left.firstEntry(), right.firstEntry());
+				assertEquals(left.lastEntry(), right.lastEntry());
+			}
+			int lsize = left.size();
+			assertEquals(lsize, right.size());
+
+			assertEquals(left.comparator(), right.comparator());
+			assertEquals(left, right);
+
+			assertEquals(left.keySet(), right.keySet());
+			assertEquals(left.entrySet(), right.entrySet());
+			ImmutableNavigableSetTest.assertSetEquality(left.navigableKeySet(), right.navigableKeySet());
+			ImmutableNavigableSetTest.assertSetEquality(left.descendingKeySet(), right.descendingKeySet());
+
+			ImmutableNavigableSetTest.compareIterators(left.entrySet().iterator(), right.entrySet().iterator());
+			ImmutableNavigableSetTest.compareIterators(left.values().iterator(), right.values().iterator());
+		} catch (Throwable e) {
+			System.out.println("Equality checkings maps of " + left + " (" + left.getClass().getSimpleName() + " / "
+					+ left.comparator() + ") - " + right + " (" + right.getClass().getSimpleName() + " / "
+					+ right.comparator() + ")");
+			throw e;
 		}
 	}
 
--- test/src/testing/saker/build/tests/utils/ImmutableNavigableSetTest.java
+++ test/src/testing/saker/build/tests/utils/ImmutableNavigableSetTest.java
@@ -16,8 +16,13 @@
 package testing.saker.build.tests.utils;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.NavigableSet;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import saker.util.ImmutableUtils;
 import saker.util.ObjectUtils;
@@ -29,67 +34,174 @@ public class ImmutableNavigableSetTest extends SakerTestCase {
 
 	@Override
 	public void runTest(Map<String, String> parameters) {
-		NavigableSet<Integer> actualset = ObjectUtils.newTreeSet(0, 1, 3, 4, 9, 10, 14);
+		{
+			NavigableSet<Integer> actualset = ObjectUtils.newTreeSet(0, 1, 3, 4, 9, 10, 14);
+			NavigableSet<Integer> reverseset = new TreeSet<>(Comparator.reverseOrder());
+			reverseset.addAll(actualset);
 
-		testSets(actualset, ImmutableUtils.unmodifiableNavigableSet(actualset), -5, 20);
-		testSets(actualset, ImmutableUtils.makeImmutableNavigableSet(actualset), -5, 20);
+			testSetRange(actualset);
+			testSetRange(reverseset);
+		}
+		{
+			//singleton checks
+			NavigableSet<Integer> singleactualset = ObjectUtils.newTreeSet(5);
+			NavigableSet<Integer> singlereverseset = new TreeSet<>(Comparator.reverseOrder());
+			singlereverseset.addAll(singleactualset);
+			testSetRange(singleactualset);
+			testSetRange(singlereverseset);
 
-		testSets(actualset, ImmutableUtils.unmodifiableNavigableSet(actualset.toArray(new Integer[0])), -5, 20);
-		testSets(actualset, ImmutableUtils.makeImmutableNavigableSet(actualset.toArray(new Integer[0])), -5, 20);
+			assertSetEquality(singleactualset, ImmutableUtils.singletonNavigableSet(5, singleactualset.comparator()));
+			assertSetEquality(singlereverseset, ImmutableUtils.singletonNavigableSet(5, singlereverseset.comparator()));
+		}
 
 		assertTrue(ImmutableUtils.singletonNavigableSet("a", String::compareToIgnoreCase).contains("A"));
 		assertTrue(ImmutableUtils.singletonNavigableSet("a", String::compareToIgnoreCase).contains("a"));
+
+		assertSetEquality(new TreeSet<>(), ImmutableUtils.emptyNavigableSet(null));
+		assertSetEquality(Collections.emptyNavigableSet(), ImmutableUtils.emptyNavigableSet(null));
+		assertSetEquality(new TreeSet<>(Comparator.reverseOrder()),
+				ImmutableUtils.emptyNavigableSet(Comparator.reverseOrder()));
+		assertSetEquality(new TreeSet<>(Comparator.naturalOrder()),
+				ImmutableUtils.emptyNavigableSet(Comparator.naturalOrder()));
+		assertSetEquality(new TreeSet<>(Comparator.reverseOrder().reversed()),
+				ImmutableUtils.emptyNavigableSet(Comparator.reverseOrder().reversed()));
 	}
 
-	protected static void testSets(NavigableSet<Integer> actualset, NavigableSet<Integer> set, int start, int end) {
-		assertSetEquality(set, actualset);
-		for (int i = start; i < end; i++) {
-			assertEquals(set.contains(i), actualset.contains(i));
-			assertEquals(set.floor(i), actualset.floor(i));
-			assertEquals(set.lower(i), actualset.lower(i));
-			assertEquals(set.ceiling(i), actualset.ceiling(i));
-			assertEquals(set.higher(i), actualset.higher(i));
+	private static void testSetRange(NavigableSet<Integer> set) {
+		int start = -5;
+		int end = 20;
 
-			assertSetEquality(set.headSet(i, false), actualset.headSet(i, false));
-			assertSetEquality(set.headSet(i, true), actualset.headSet(i, true));
-			assertSetEquality(set.tailSet(i, false), actualset.tailSet(i, false));
-			assertSetEquality(set.tailSet(i, true), actualset.tailSet(i, true));
+		System.out.println("ImmutableNavigableSetTest.testSetRange() " + set + " cmp: " + set.comparator());
+
+		testSets(set, ImmutableUtils.unmodifiableNavigableSet(set), start, end);
+		testSets(set, ImmutableUtils.makeImmutableNavigableSet(set), start, end);
+
+		Integer[] elemsarray = set.toArray(new Integer[0]);
+		Integer[] elemslargearray = set.toArray(new Integer[set.size() + 128]);
+
+		testSets(set, ImmutableUtils.unmodifiableNavigableSet(elemsarray, set.comparator()), start, end);
+		testSets(set, ImmutableUtils.unmodifiableNavigableSet(elemslargearray, 0, set.size(), set.comparator()), start,
+				end);
+		testSets(set, ImmutableUtils.unmodifiableNavigableSet(Arrays.asList(elemsarray), set.comparator()), start, end);
+		testSets(set, ImmutableUtils.makeImmutableNavigableSet(elemsarray, set.comparator()), start, end);
+	}
+
+	protected static void testSets(NavigableSet<Integer> actualset, NavigableSet<Integer> utilset, int start, int end) {
+		assertSetEquality(utilset, actualset);
+		for (int i = start; i < end; i++) {
+			try {
+				assertEquals(utilset.contains(i), actualset.contains(i));
+				assertEquals(utilset.floor(i), actualset.floor(i));
+				assertEquals(utilset.lower(i), actualset.lower(i));
+				assertEquals(utilset.ceiling(i), actualset.ceiling(i));
+				assertEquals(utilset.higher(i), actualset.higher(i));
+
+				assertSetEqualityImpl(utilset.headSet(i), actualset.headSet(i));
+				assertSetEqualityImpl(utilset.tailSet(i), actualset.tailSet(i));
+				assertSetEquality(utilset.headSet(i, false), actualset.headSet(i, false));
+				assertSetEquality(utilset.headSet(i, true), actualset.headSet(i, true));
+				assertSetEquality(utilset.tailSet(i, false), actualset.tailSet(i, false));
+				assertSetEquality(utilset.tailSet(i, true), actualset.tailSet(i, true));
+			} catch (Throwable e) {
+				System.out.println("With value: " + i);
+				throw e;
+			}
 			for (int j = i; j < end; j++) {
-				assertSetEquality(set.subSet(i, false, j, false), actualset.subSet(i, false, j, false));
-				assertSetEquality(set.subSet(i, true, j, false), actualset.subSet(i, true, j, false));
-				assertSetEquality(set.subSet(i, false, j, true), actualset.subSet(i, false, j, true));
-				assertSetEquality(set.subSet(i, true, j, true), actualset.subSet(i, true, j, true));
+				int l = i;
+				int r = j;
+				Comparator<? super Integer> comparator = ObjectUtils.getComparator(utilset);
+				if (comparator.compare(l, r) > 0) {
+					//use reverse order if the values compare otherwise
+					l = j;
+					r = i;
+				}
+
+				assertSetEqualityImpl(utilset.subSet(l, r), actualset.subSet(l, r));
+				assertSetEquality(utilset.subSet(l, false, r, false), actualset.subSet(l, false, r, false));
+				assertSetEquality(utilset.subSet(l, true, r, false), actualset.subSet(l, true, r, false));
+				assertSetEquality(utilset.subSet(l, false, r, true), actualset.subSet(l, false, r, true));
+				assertSetEquality(utilset.subSet(l, true, r, true), actualset.subSet(l, true, r, true));
 			}
 		}
 	}
 
-	private static void assertSetEquality(NavigableSet<?> left, NavigableSet<?> right) {
-		assertSetEqualityImpl(left, right);
-		assertSetEqualityImpl(left.descendingSet(), right.descendingSet());
+	public static void assertSetEquality(NavigableSet<?> left, NavigableSet<?> right) {
+		try {
+			assertSetEqualityImpl(left, right);
+		} catch (Throwable e) {
+			System.out.println("Comparing set " + left + " (" + left.getClass().getSimpleName() + " / "
+					+ left.comparator() + ") - " + right + " (" + right.getClass().getSimpleName() + " / "
+					+ right.comparator() + ")");
+			throw e;
+		}
+
+		try {
+			assertSetEqualityImpl(left.descendingSet(), right.descendingSet());
+		} catch (Throwable e) {
+			System.out.println("Comparing descending set of " + left + " (" + left.getClass().getSimpleName() + " / "
+					+ left.comparator() + ") - " + right + " (" + right.getClass().getSimpleName() + " / "
+					+ right.comparator() + ")");
+			throw e;
+		}
+
+		try {
+			assertSetEqualityImpl(left.descendingSet().descendingSet(), right.descendingSet().descendingSet());
+		} catch (Throwable e) {
+			System.out.println("Comparing descending 2 set of " + left + " (" + left.getClass().getSimpleName() + " / "
+					+ left.comparator() + ") - " + right + " (" + right.getClass().getSimpleName() + " / "
+					+ right.comparator() + ")");
+			throw e;
+		}
 	}
 
-	private static void assertSetEqualityImpl(NavigableSet<?> left, NavigableSet<?> right) throws AssertionError {
-		boolean lempty = left.isEmpty();
-		assertEquals(lempty, right.isEmpty());
-		if (!lempty) {
-			assertEquals(left.first(), right.first());
-			assertEquals(left.last(), right.last());
+	private static void assertSetEqualityImpl(SortedSet<?> left, SortedSet<?> right) throws AssertionError {
+		try {
+			boolean lempty = left.isEmpty();
+			assertEquals(lempty, right.isEmpty());
+			if (!lempty) {
+				assertEquals(left.first(), right.first());
+				assertEquals(left.last(), right.last());
+			}
+			int lsize = left.size();
+			assertEquals(lsize, right.size());
+
+			assertEquals(left.comparator(), right.comparator());
+			assertEquals(left, right);
+			assertEquals(left.toArray(), right.toArray());
+			assertEquals(left.toArray(new Object[lsize]), right.toArray(new Object[lsize]));
+
+			Object[] larray = new Object[lsize + 2];
+			Object[] rarray = new Object[lsize + 2];
+			Arrays.fill(larray, "f");
+			Arrays.fill(rarray, "f");
+			assertEquals(left.toArray(larray), right.toArray(rarray));
+
+			assertTrue(ObjectUtils.collectionOrderedEquals(left, right));
+
+			compareIterators(left.iterator(), right.iterator());
+
+			if (left instanceof NavigableSet && right instanceof NavigableSet) {
+				NavigableSet<?> lnavset = (NavigableSet<?>) left;
+				NavigableSet<?> rnavset = (NavigableSet<?>) right;
+				compareIterators(lnavset.descendingIterator(), rnavset.descendingIterator());
+			}
+		} catch (Throwable e) {
+			System.out.println("Equality checkings sets of " + left + " (" + left.getClass().getSimpleName() + " / "
+					+ left.comparator() + ") - " + right + " (" + right.getClass().getSimpleName() + " / "
+					+ right.comparator() + ")");
+			throw e;
 		}
-		assertEquals(left.size(), right.size());
+	}
 
-		assertEquals(left.comparator(), right.comparator());
-		assertEquals(left, right);
-		assertEquals(left.toArray(), right.toArray());
-		int size = left.size();
-		assertEquals(left.toArray(new Object[size]), right.toArray(new Object[size]));
+	public static void compareIterators(Iterator<?> l, Iterator<?> r) {
+		while (l.hasNext()) {
+			assertTrue(r.hasNext());
 
-		Object[] larray = new Object[size + 2];
-		Object[] rarray = new Object[size + 2];
-		Arrays.fill(larray, "f");
-		Arrays.fill(rarray, "f");
-		assertEquals(left.toArray(larray), right.toArray(rarray));
-
-		assertTrue(ObjectUtils.collectionOrderedEquals(left, right));
+			Object n1 = l.next();
+			Object n2 = r.next();
+			assertEquals(n1, n2);
+		}
+		assertFalse(r.hasNext());
 	}
 
 }
